298c787a16a231eca6faa820c591b7f6078d9339
Hafiz Ismail <sgk.sprm@gmail.com>
2015-05-03 11:35:01 +0800

    Implemented a utility function to parse `Accept` header media types - Separate `utils` package - Unit tested with 100% coverage

---
+++ server/utils/parse_accept_headers.go
@@ -0,0 +1,91 @@
+package utils
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type MediaTypeParams map[string]string
+type MediaType struct {
+	String     string          `json:"string"`
+	Type       string          `json:"type"`
+	Tree       string          `json:"tree"`
+	SubType    string          `json:"subtype"`
+	Suffix     string          `json:"suffix"`
+	Parameters MediaTypeParams `json:"parameters"`
+}
+type AcceptHeader struct {
+	MediaType     MediaType `json:"media_type"`
+	QualityFactor float64   `json:"quality_factor"`
+}
+
+type AcceptHeaders []AcceptHeader
+
+func ParseAcceptHeaders(str string) AcceptHeaders {
+
+	var headers AcceptHeaders
+
+	// parses `application/vnd.api+json;q=0.8;version=1.0` into MediaType type
+	parseMediaType := func(str string) MediaType {
+
+		var mediaType MediaType
+
+		str = strings.Replace(str, " ", "", -1)
+		tokens := strings.Split(str, ";")
+
+		mediaType.String = tokens[0]
+
+		// if params exists, parse params; else params is nil
+		paramsTokens := tokens[1:]
+		if len(paramsTokens) > 0 && paramsTokens[0] != "" {
+			mediaType.Parameters = map[string]string{}
+			for _, paramsToken := range paramsTokens {
+				p := strings.Split(paramsToken, "=")
+				if len(p) == 1 {
+					mediaType.Parameters[p[0]] = ""
+				}
+				if len(p) > 1 {
+					mediaType.Parameters[p[0]] = p[1]
+				}
+			}
+		}
+
+		// match ((type)/(subtype)((+)(suffix))?)
+		r, _ := regexp.Compile("^([\\w\\*\\-]+)\\/([\\w\\*\\.\\-]+)((\\+)(\\w+))?")
+		match := r.FindStringSubmatch(mediaType.String)
+		if len(match) == 0 {
+			return mediaType
+		}
+
+		// successful match results len() always 6
+		mediaType.Type = match[1]
+		mediaType.SubType = match[2]
+		mediaType.Suffix = match[5]
+
+		// parse [tree .] sub-type
+		treeStr := strings.Split(mediaType.SubType, ".")
+		if len(treeStr) > 1 && treeStr[0] != "" {
+			mediaType.Tree = treeStr[0]
+			mediaType.SubType = strings.Join(treeStr[1:], ".")
+		}
+
+		return mediaType
+	}
+
+	str = strings.Replace(str, " ", "", -1)
+	mediaTypes := strings.Split(str, ",")
+	for _, mediaTypeStr := range mediaTypes {
+		mediaType := parseMediaType(mediaTypeStr)
+		header := AcceptHeader{mediaType, 1}
+		if len(mediaType.Parameters["q"]) > 0 {
+			q, err := strconv.ParseFloat(mediaType.Parameters["q"], 64)
+			if err != nil {
+				q = 1 // default `q` value
+			}
+			header.QualityFactor = q
+		}
+		headers = append(headers, header)
+	}
+	return headers
+}
---
+++ server/utils/parse_accept_headers_test.go
@@ -0,0 +1,206 @@
+package utils_test
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/sogko/golang-rest-api-server-example/server/utils"
+)
+
+var _ = Describe("ParseAcceptHeaders", func() {
+
+	type testMap struct {
+		TestValue       string
+		ExpectedLen     int
+		ExpectedResults AcceptHeaders
+	}
+	type testMaps []testMap
+
+	var tests = testMaps{
+		testMap{
+			TestValue: "",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{}, 1},
+			},
+		},
+		testMap{
+			TestValue: ";",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{}, 1},
+			},
+		},
+		testMap{
+			TestValue: ";q=",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					Parameters: MediaTypeParams{"q": ""},
+				}, 1},
+			},
+		},
+		testMap{
+			TestValue: "application/json",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:  "application/json",
+					Type:    "application",
+					Tree:    "",
+					SubType: "json",
+					Suffix:  "",
+				}, 1},
+			},
+		},
+		testMap{
+			TestValue: "application/json;q=",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:     "application/json",
+					Type:       "application",
+					Tree:       "",
+					SubType:    "json",
+					Suffix:     "",
+					Parameters: MediaTypeParams{"q": ""},
+				}, 1},
+			},
+		},
+		testMap{
+			TestValue: "application/json;q",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:     "application/json",
+					Type:       "application",
+					Tree:       "",
+					SubType:    "json",
+					Suffix:     "",
+					Parameters: MediaTypeParams{"q": ""},
+				}, 1},
+			},
+		},
+		testMap{
+			TestValue: "application/json;  q=0.9 ",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:     "application/json",
+					Type:       "application",
+					Tree:       "",
+					SubType:    "json",
+					Suffix:     "",
+					Parameters: MediaTypeParams{"q": "0.9"},
+				}, 0.9},
+			},
+		},
+		testMap{
+			TestValue: "application/vnd.sgk.v1+json ",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:  "application/vnd.sgk.v1+json",
+					Type:    "application",
+					Tree:    "vnd",
+					SubType: "sgk.v1",
+					Suffix:  "json",
+				}, 1},
+			},
+		},
+		testMap{
+			TestValue: "application/vnd.sgk.v1+json;q=0.8",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:  "application/vnd.sgk.v1+json",
+					Type:    "application",
+					Tree:    "vnd",
+					SubType: "sgk.v1",
+					Suffix:  "json",
+					Parameters: MediaTypeParams{
+						"q": "0.8",
+					},
+				}, 0.8},
+			},
+		},
+		testMap{
+			TestValue: "application/vnd.sgk+json; q=0.8 ;version=1.0",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:  "application/vnd.sgk+json",
+					Type:    "application",
+					Tree:    "vnd",
+					SubType: "sgk",
+					Suffix:  "json",
+					Parameters: MediaTypeParams{
+						"q":       "0.8",
+						"version": "1.0",
+					},
+				}, 0.8},
+			},
+		},
+		testMap{
+			TestValue: "application/vnd.sgk.rest-api-server.v1+json; q=0.8 ;version=1.0,*/*",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:  "application/vnd.sgk.rest-api-server.v1+json",
+					Type:    "application",
+					Tree:    "vnd",
+					SubType: "sgk.rest-api-server.v1",
+					Suffix:  "json",
+					Parameters: MediaTypeParams{
+						"q":       "0.8",
+						"version": "1.0",
+					},
+				}, 0.8},
+				AcceptHeader{MediaType{
+					String:  "*/*",
+					Type:    "*",
+					Tree:    "",
+					SubType: "*",
+					Suffix:  "",
+				}, 1},
+			},
+		},
+		testMap{
+			TestValue: "application/vnd.sgk+json; q=0.8 ;version=1.0,application/json , */*;q=noninteger",
+			ExpectedResults: AcceptHeaders{
+				AcceptHeader{MediaType{
+					String:  "application/vnd.sgk+json",
+					Type:    "application",
+					Tree:    "vnd",
+					SubType: "sgk",
+					Suffix:  "json",
+					Parameters: MediaTypeParams{
+						"q":       "0.8",
+						"version": "1.0",
+					},
+				}, 0.8},
+				AcceptHeader{MediaType{
+					String:  "application/json",
+					Type:    "application",
+					Tree:    "",
+					SubType: "json",
+					Suffix:  "",
+				}, 1},
+				AcceptHeader{MediaType{
+					String:  "*/*",
+					Type:    "*",
+					Tree:    "",
+					SubType: "*",
+					Suffix:  "",
+					Parameters: MediaTypeParams{
+						"q": "noninteger",
+					},
+				}, 1},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		testValue := test.TestValue
+		expectedResults := test.ExpectedResults
+		Context(fmt.Sprintf("when `Accept=%v`", testValue), func() {
+			result := ParseAcceptHeaders(testValue)
+			It("parses OK", func() {
+				Expect(len(result)).To(Equal(len(expectedResults)))
+				for i, _ := range expectedResults {
+					Expect(result[i].MediaType).To(Equal(expectedResults[i].MediaType))
+					Expect(result[i].QualityFactor).To(Equal(expectedResults[i].QualityFactor))
+				}
+			})
+		})
+	}
+})
